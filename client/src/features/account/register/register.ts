d31083bca0c727fa4eddfcdaf29fe916c4417eba
Mohamed <mahmedprof@gmail.com>
2025-09-12 16:13:56 +0300

    Client login and registration

---
+++ client/src/features/account/register/register.ts
@@ -0,0 +1,29 @@
+import { Component, inject, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RegisterCreds, User } from '../../../types/user';
+import { AccountService } from '../../../core/services/account-service';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+  protected creds = {} as RegisterCreds;
+  private accountService = inject(AccountService);
+  cancelRegister = output<boolean>();
+  register() {
+    this.accountService.register(this.creds).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.cancel();
+      },
+      error: (error) => console.error(),
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
+import { User } from '../../types/user';
 @Component({
   selector: 'app-nav',
   imports: [FormsModule],
@@ -7,9 +9,19 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './nav.css',
 })
 export class Nav {
+  protected accountService = inject(AccountService);
+
   protected creds: any = {};
 
   login() {
-    console.log(this.creds);
+    this.accountService.login(this.creds).subscribe({
+      next: (result) => {
+        this.creds = {};
+      },
+      error: (error) => alert(error.message),
+    });
+  }
+  logout() {
+    this.accountService.logout();
   }
 }
